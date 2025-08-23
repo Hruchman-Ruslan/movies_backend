5d67751012d2bbc5e1574574189d8ed776036f2f
Ruslan <Giovingo92@gmail.com>
2025-08-23 16:46:30 +0300

    feat: basic code absolute patch

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+
+import { AppModule } from '@app/app.module';
 
 async function bootstrap() {
+  if (!process.env.IS_TS_NODE) {
+    await import('module-alias/register');
+  }
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3030);
 }
 bootstrap();
