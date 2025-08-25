cb43540060a1711f668f01b5923f2cdb4ac07169
Ruslan <Giovingo92@gmail.com>
2025-08-25 23:15:14 +0300

    feat: pagination

---
+++ src/movies/dto/pagination.dto.ts
@@ -0,0 +1,9 @@
+import { IsInt, Min } from 'class-validator';
+import { Type } from 'class-transformer';
+
+export class PaginationDto {
+  @Type(() => Number)
+  @IsInt()
+  @Min(1)
+  page: number = 1;
+}
--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -12,13 +12,13 @@ import { TmdbMovie } from './types/tmdb.types';
 export class MoviesService {
   constructor(private readonly configService: ConfigService) {}
 
-  async getPopularMovies(): Promise<MovieDto[]> {
+  async getPopularMovies(page = 1): Promise<MovieDto[]> {
     const apiKey = this.configService.get<string>('TMDB_API_KEY')!;
     const baseUrl = this.configService.get<string>('TMDB_BASE_URL')!;
     const imageBaseUrl = this.configService.get<string>('TMDB_IMAGE_BASE_URL')!;
 
     const genresMap = await fetchGenres(apiKey, baseUrl);
-    const { results } = await fetchPopularMovies(apiKey, baseUrl);
+    const { results } = await fetchPopularMovies(apiKey, baseUrl, page);
 
     return results.map((movie: TmdbMovie) =>
       mapMovieToDto(movie, genresMap, imageBaseUrl),
